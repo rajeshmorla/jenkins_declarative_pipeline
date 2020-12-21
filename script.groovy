9847102a2eccf2dc9cfa58a5efad52a4375b0917
Rajesh Morla <rajeshmorla@live.com>
2020-12-21 16:11:22 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,7 @@
 def buildApp() {
-    echo 'building the application...'
+    echo "building the application"
+    
+    println('In groovy')
     
     RESTClient client = new RESTClient("https://calendarific.com/api/v2/holidays?&api_key=758f54db8c52c2b500c928282fe83af1b1aa2be8&country=IN&year=2020")
     def response
