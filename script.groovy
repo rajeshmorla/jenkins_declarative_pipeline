c9128e5844faea56cbcf0e6bd13b4f739add18be
Rajesh Morla <rajeshmorla@live.com>
2020-12-21 16:13:18 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -10,13 +10,15 @@ def buildApp() {
         assert response.statusCode == 200
         
         println('Response: '+response.content)
+        
+            
+        def res = httpRequest "https://calendarific.com/api/v2/holidays?&api_key=758f54db8c52c2b500c928282fe83af1b1aa2be8&country=IN&year=2020"
+        println('Status: '+res.status)
+        println('Response: '+res.content)
     } catch (RESTClientException e) {
         assert e?.response?.statusCode != 200
     }
-    
-    def response = httpRequest "https://calendarific.com/api/v2/holidays?&api_key=758f54db8c52c2b500c928282fe83af1b1aa2be8&country=IN&year=2020"
-    println('Status: '+response.status)
-    println('Response: '+response.content)
+
 } 
 
 return this
